c3eaa64411658e6ef9abae352d351e9765cef8be
Pawan Chawla <171932023+Pawan116@users.noreply.github.com>
2026-04-24 02:23:26 +0530

    Corrected backend output

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 
 app.get("/", (req, res) => {
-  res.send("LoadLink API Running...");
+  res.send("LoadLink backend is live");
 });
 
 const PORT = process.env.PORT || 5000;
